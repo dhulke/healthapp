c2ad65cf3a876567da19c2ea87b9521931e7e969
danilo.moraes <danilo.moraes@trydatabook.com>
2023-06-21 20:00:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Specify the port number you want to use
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/health', (req, res) => {
+    res.send('Healthy!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
